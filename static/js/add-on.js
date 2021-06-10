0a8e984f24154d1a7efccf9d8c9dfd6ab8d5e95d
James Molyneux <molyneuj@oregonstate.edu>
2021-06-10 03:15:02 -0700

    Commit Data and Static folders

---
+++ static/js/add-on.js
@@ -0,0 +1,9 @@
+/*
+  This js file is for individual users to modify the scripts for their personal site,
+  or for the implementation of features specifically for their site. Anything that
+  is an official part of the theme (ex. Pull Requests) should be included in main.js
+  and follow the formatting and style given.
+*/
+<script type="text/javascript"
+  src="https://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML">
+</script>
